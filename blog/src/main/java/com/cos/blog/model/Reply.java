134c23ac7a1a7c1cc09eaeba6f18aa5af71d05e4
nuguseyo024 <nuguseyo024@naver.com>
2022-09-10 15:11:32 +0900

    22-09-10 스프링부트 강좌 22강(블로그 프로젝트) - 연관관계의 주인

--- blog/src/main/java/com/cos/blog/model/Reply.java
+++ blog/src/main/java/com/cos/blog/model/Reply.java
@@ -1,14 +1,17 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -34,9 +37,12 @@ public class Reply {
 	@JoinColumn(name="boardId")
 	private Board board;
 	
-	@ManyToOne // 여러개의 답글을 하나의 유저가 쓸 수 있다  
+	@ManyToOne(fetch = FetchType.EAGER) // 여러개의 답글을 하나의 유저가 쓸 수 있다  
 	@JoinColumn(name="userId")
 	private User user;
+	
+	@OneToMany(mappedBy = "board",fetch = FetchType.EAGER)
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
