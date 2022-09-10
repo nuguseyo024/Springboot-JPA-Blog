7b0256300db38ee2429474003e2819b3f2ac988f
nuguseyo024 <nuguseyo024@naver.com>
2022-09-10 19:46:21 +0900

    스프링부트 강좌 27강(블로그 프로젝트) - 전체 select 및 paging 테스트

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,20 @@ public class DummyControllerTest {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/dummy/users")	
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한 페이지당 2건의 데이터를 return 받기 
+	@GetMapping("/dummy/user")	
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 	
 	@GetMapping("/dummy/user/{id}")	// {id} 주소로 파라미터를 전달받을 수 있음 
 	public User detail(@PathVariable int id) {
