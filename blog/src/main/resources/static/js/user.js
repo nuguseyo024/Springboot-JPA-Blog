8541951b81277b5441895aae4f6275f52dc7f4de
nuguseyo024 <nuguseyo024@naver.com>
2022-09-14 15:50:25 +0900

    스프링부트 강좌 38강(블로그 프로젝트) - 회원가입 하기 Ajax요청

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -12,10 +12,28 @@ let index = {
 			password: $("#password").val(),
 			email: $("#email").val()
 			
-		}
-		// console.log(data);
+		};
+		// console.log(data);$
+		
 		// ajax 통신을 이용해 3개의 데이터를 json 으로 변경하여 insert 요청 
-		$.ajax().done.fail();
+		// ajax 호출 시 default 가 비동기 호출 
+		$.ajax({
+			// 회원가입 수행 요청 
+			type:"POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data),
+			contentType:"application/json; charset=utf-8",
+			// body 데이터가 어떤 타입인지 (MIME)
+			dataType: "json" 
+		}).done(function(resp){
+			// 응답 결과가 정상이면 done 실행 
+			alert("회원가입이 완료되었습니다. ");
+			console.log(resp);
+			location.href="/blog";
+		}).fail(function(error){
+			// 응답 결과가 비정상이면 fail 실행 
+			alert(JSON.stringify(error));
+		});
 		
 	}  
 }
