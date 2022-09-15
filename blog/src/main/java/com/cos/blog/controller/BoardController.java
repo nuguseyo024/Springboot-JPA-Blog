d9a6c9ee439f78b03a995b99e3b1da3242aef284
nuguseyo024 <nuguseyo024@naver.com>
2022-09-15 17:32:30 +0900

    49강 오류 해결 (WebSecurityConfigurerAdapter 지원 중단 이슈)

--- blog/src/main/java/com/cos/blog/controller/BoardController.java
+++ blog/src/main/java/com/cos/blog/controller/BoardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 	
 	@GetMapping({"","/"})
-	public String indedx() {
+	public String index() {
 		return "index";
 	}
 }
