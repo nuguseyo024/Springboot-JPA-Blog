4dcf7f518d3c7f707ca4d83c2c0c40337bd02478
nuguseyo024 <nuguseyo024@naver.com>
2022-09-25 22:59:29 +0900

    스프링부트 강좌69강(블로그 프로젝트 ) - 댓글 작성시 Dto 사용해보기

--- blog/src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ blog/src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -40,6 +40,8 @@ public class BoardApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 	
+	// 데이터를 받을 때 컨트롤러에서 dto를 만들어서 받는 게 좋다.
+	// 프로젝트가 거대해지면 데이터도 많아지고, 필드들도 많아지고 조인도 많아지고 
 	@PostMapping("/api/board/{boardId}/reply")
 	public ResponseDto<Integer> replySave(@PathVariable int boardId, @RequestBody Reply reply, @AuthenticationPrincipal PrincipalDetail principal) {		
 		boardService.replyWrite(principal.getUser(),boardId, reply);
